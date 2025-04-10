6b43364dec52b71667a491f6799d573fa3198061
Felipe <felipeagle@hotmail.com>
2025-04-10 12:36:12 -0300

    feat: add migration for sale and saleitem

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Migrations/20250409190502_CreateSalesAndSaleItemsTables.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateSalesAndSaleItemsTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "sales",
+                columns: table => new
+                {
+                    sale_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    sale_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    customer_id = table.Column<string>(type: "text", nullable: false),
+                    customer_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    branch_id = table.Column<string>(type: "text", nullable: false),
+                    branch_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    total_amount = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    is_cancelled = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sales", x => x.sale_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    Username = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Password = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Role = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Status = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sale_items",
+                columns: table => new
+                {
+                    sale_item_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    sale_id = table.Column<int>(type: "integer", nullable: false),
+                    product_id = table.Column<string>(type: "text", nullable: false),
+                    product_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    unit_price = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    discount = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    total_item_amount = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sale_items", x => x.sale_item_id);
+                    table.ForeignKey(
+                        name: "FK_sale_items_sales_sale_id",
+                        column: x => x.sale_id,
+                        principalTable: "sales",
+                        principalColumn: "sale_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_sale_items_sale_id",
+                table: "sale_items",
+                column: "sale_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "sale_items");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "sales");
+        }
+    }
+}
